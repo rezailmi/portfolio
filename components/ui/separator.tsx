9f23ebddb0479586e99b9a61277e9160679cd5f5
Reza Ilmi <hi.rezailmi@gmail.com>
2026-01-05 08:20:06 +0800

    feat: migrate Batch 1 components to Base UI (label, separator, progress)
    - Update label to use Field.Label from @base-ui/react/field
    - Update separator to use @base-ui/react/separator
    - Update progress to use @base-ui/react/progress with Track wrapper
    - Add Base UI library config to components.json
    - Note: aspect-ratio kept as Radix UI (no Base UI equivalent)
    
    All components tested and build succeeds.

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,21 +1,20 @@
 "use client"
 
 import * as React from "react"
-import * as SeparatorPrimitive from "@radix-ui/react-separator"
+import { Separator as SeparatorPrimitive } from "@base-ui/react/separator"
 
 import { cn } from "@/lib/utils"
 
 const Separator = React.forwardRef<
-  React.ElementRef<typeof SeparatorPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+  React.ElementRef<typeof SeparatorPrimitive>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive>
 >(
   (
-    { className, orientation = "horizontal", decorative = true, ...props },
+    { className, orientation = "horizontal", ...props },
     ref
   ) => (
-    <SeparatorPrimitive.Root
+    <SeparatorPrimitive
       ref={ref}
-      decorative={decorative}
       orientation={orientation}
       className={cn(
         "shrink-0 bg-border",
@@ -26,6 +25,6 @@ const Separator = React.forwardRef<
     />
   )
 )
-Separator.displayName = SeparatorPrimitive.Root.displayName
+Separator.displayName = "Separator"
 
 export { Separator }
