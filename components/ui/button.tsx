5b49002e84b1dc1c31c04a32b32f08f4848723ef
rezailmi <hi.rezailmi@gmail.com>
2026-07-02 18:25:36 +0800

    refactor: migrate Button from asChild to Base UI render prop

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
+import { useRender } from "@base-ui/react/use-render"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
@@ -34,21 +34,22 @@ const buttonVariants = cva(
 )
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "color">,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  render?: useRender.RenderProp
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
+  ({ className, variant, size, render, ...props }, ref) => {
+    return useRender({
+      defaultTagName: "button",
+      render,
+      props: {
+        className: cn(buttonVariants({ variant, size, className })),
+        ...props,
+      },
+      ref,
+    })
   }
 )
 Button.displayName = "Button"
